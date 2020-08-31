3e3c273145dbe43064455cb0eba073f05ca1d036
Tan Jiaqing <jiaqing.tan4@gmail.com>
2020-09-01 00:13:05 +0800

    Add Health and Damage scripts

---
+++ Assets/Scripts/Damage/DamageScript.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour
+{
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private bool isFriendly;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If friendly projectile, check for "Enemies" tag
+        // Else, it is enemy's projectile, check for "Player" tag
+
+        if (isFriendly)
+        {
+            if(other.gameObject.CompareTag("Enemies"))
+            {
+                other.gameObject.GetComponent<HealthScript>().ApplyDamage(damage);
+            }
+        }
+        else
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.GetComponent<HealthScript>().ApplyDamage(damage);
+            }
+        }
+    }
+
+    //Getters/Setters
+    public float GetDamage()
+    {
+        return damage;
+    }    
+
+    public void SetDamage(float damage)
+    {
+        this.damage = damage;
+    }
+
+    public void SetIsFriendly(bool isFriendly)
+    {
+        this.isFriendly = isFriendly;
+    }
+}
