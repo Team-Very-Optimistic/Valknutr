0823225fae60ea9e5dcd01ba5bae76b154741562
vuhieunguyen <hieunguyen98pro@gmail.com>
2020-09-03 06:23:11 +0800

    Added a sample UI scene.  Added VFX assets

---
+++ Assets/Scripts/SkillCooldown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCooldown : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public bool isCooldown;
+
+    public float skillCooldown { get; set; }
+
+    public void RestartSkill()
+    {
+        slider.maxValue = skillCooldown;
+        slider.value = skillCooldown;
+        isCooldown = false;
+    }
+
+    public void UpdateSlider()
+    {
+        float sliderChange = Time.deltaTime;
+        slider.value -= sliderChange;
+        if (slider.value <= 0)
+        {
+            RestartSkill();
+        }
+    }
+
+}
