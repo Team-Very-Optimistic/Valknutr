3f0135632b8a56b937076bd6e55b3ed928fc8071
Caleb <sonicheze8@gmail.com>
2020-09-06 15:53:46 +0800

    Add slots

--- Assets/Scripts/UI/CraftMenuManager.cs
+++ Assets/Scripts/UI/CraftMenuManager.cs
@@ -1,24 +1,22 @@
 ﻿using UnityEngine;
 
+public class CraftMenuManager : Singleton<CraftMenuManager>
+{
+    public GameObject craftMenu;
+    public DisplaySpells displaySpells;
 
-
-    public class CraftMenuManager : Singleton<CraftMenuManager>
+    public void AddItem(SpellItem spellItem)
     {
-        public GameObject craftMenu;
-        public DisplaySpells displaySpells;
-
-        public void AddItem(SpellItem spellItem)
-        {
-            displaySpells.AddItem(spellItem);
-        }
-        
-        public void Display()
-        {
-            craftMenu.SetActive(!craftMenu.activeSelf);
-        }
-
-        public void RemoveItem(SpellItem spellItem)
-        {
-            displaySpells.RemoveItem(spellItem);
-        }
+        displaySpells.AddItem(spellItem);
     }
+    
+    public void Display()
+    {
+        craftMenu.SetActive(!craftMenu.activeSelf);
+    }
+
+    public void RemoveItem(SpellItem spellItem)
+    {
+        displaySpells.RemoveItem(spellItem);
+    }
+}
--- Assets/Scripts/UI/UISlot.cs
+++ Assets/Scripts/UI/UISlot.cs
@@ -3,13 +3,14 @@ using UnityEngine.EventSystems;
 
 class UISlot : MonoBehaviour, IDropHandler
 {
+    private UIItem slottedItem;
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData != null)
         {
             var uiItem = eventData.pointerDrag.GetComponent<UIItem>();
             if (uiItem == null) return;
-            
+            slottedItem = uiItem;
             uiItem.isSlotted = true;
             var rectTransform = eventData.pointerDrag.GetComponent<RectTransform>();
             rectTransform.SetParent(transform, true);
