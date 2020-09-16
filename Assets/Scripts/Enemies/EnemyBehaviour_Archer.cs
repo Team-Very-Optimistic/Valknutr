bd570656383e20c97a2f55fd6b803ba0d0b0fe1b
Tan Jiaqing <jiaqing.tan4@gmail.com>
2020-09-16 18:38:49 +0800

    Complete ranged enemy

--- Assets/Scripts/Enemies/EnemyBehaviour_Archer.cs
+++ Assets/Scripts/Enemies/EnemyBehaviour_Archer.cs
@@ -6,9 +6,15 @@ using UnityEngine.Experimental.AI;
 
 public class EnemyBehaviour_Archer : EnemyBehaviourBase
 {
+    private Vector3 firingPosOffset = new Vector3(0.0f, 90.0f, 0.0f);
+    private GameObject bow;
+
+    public GameObject arrowPrefab;
+
     // Start is called before the first frame update
     public override void Start()
     {
+        bow = this.gameObject.transform.Find("Erika_Archer_Meshes").Find("Bow").gameObject;
         base.Start(); 
     }
 
@@ -20,7 +26,15 @@ public class EnemyBehaviour_Archer : EnemyBehaviourBase
         if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Running"))
         {
             //Offset rotation to properly face player when attacking
-            transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+            transform.Rotate(firingPosOffset);
         }
+    }
+
+    public void FireArrow()
+    {
+        Vector3 fireDirection = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z) - this.gameObject.transform.position;
+        Vector3 verticalOffset = new Vector3(0.0f, 1.4f, 0.0f); //bow.transform.position.y returns 0 - Model issue?
+        GameObject arrow = GameObject.Instantiate(arrowPrefab, bow.transform.position + verticalOffset, Quaternion.LookRotation(fireDirection));
+        arrow.GetComponent<EnemyProjectile>().Launch(fireDirection, 1.75f);
     }
 }
---
+++ Assets/Scripts/Enemies/EnemyProjectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class EnemyProjectile : MonoBehaviour
+{
+    public float _damage = 1;
+    public Vector3 direction;
+    public float speed;
+    public float timeToExpire = 20f;
+
+    public void Launch(Vector3 direction, float speed)
+    {
+        this.direction = direction;
+        this.speed = speed;
+        gameObject.GetComponent<Rigidbody>().velocity = direction * speed;
+        Destroy(gameObject, timeToExpire);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            var damageScript = GetComponent<Damage>();
+            damageScript.SetDamage(_damage);
+            damageScript.DealDamage(other);
+            Destroy(gameObject);
+        }
+    }
+}
