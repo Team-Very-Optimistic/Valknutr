d44f38aee633902861fc2c7ac5b1efa59a3508c4
Caleb <sonicheze8@gmail.com>
2020-09-24 16:22:51 +0800

    Refactor spells to only be for spells

---
+++ Assets/Scripts/Spell/BaseSpells/GroundStrikeBehaviour.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GroundStrikeBehaviour : SpellBehavior
+{
+    public float radius = 3.0F;
+    public float power = 50.0F;
+    private Damage damageScript;
+
+    public override void Init()
+    {
+        _damage = 1f;
+        _speed = 2f;
+        _cooldown = .2f;
+        _objectForSpell = GameManager.Instance._weapon;
+        animationType = CastAnimation.Projectile;
+        damageScript = _objectForSpell.GetComponent<Damage>();
+    }
+
+    public override void SpellBehaviour(Spell spell)
+    {
+        var position = _objectForSpell.transform.position;
+
+
+        AudioManager.PlaySoundAtPosition("projectileHit", position);
+        EffectManager.PlayEffectAtPosition("groundStrike", position);
+
+        var cols = Physics.OverlapSphere(position, radius);
+        
+        foreach (var col in cols)
+        {
+            if (!col.CompareTag("Player") &&  !col.CompareTag("Projectile") && col.attachedRigidbody != null)
+            {
+                col.attachedRigidbody.AddExplosionForce(power * _damage, position, radius, 0, ForceMode.Impulse);
+            }
+             damageScript.SetDamage(_damage);
+             damageScript.DealDamage(col);
+        }
+        
+    }
+}
