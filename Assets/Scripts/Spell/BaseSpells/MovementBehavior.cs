e29b9e94b89cd26ffb8772a77dba8de83b2c34f5
Caleb <sonicheze8@gmail.com>
2020-09-24 14:39:05 +0800

    Post processing

--- Assets/Scripts/Spell/BaseSpells/MovementBehavior.cs
+++ Assets/Scripts/Spell/BaseSpells/MovementBehavior.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityStandardAssets.Characters.ThirdPerson;
+﻿using UnityStandardAssets.Characters.ThirdPerson;
 
 class MovementBehavior : SpellBehavior
 {
     private ThirdPersonCharacter _controller;
-    private float _moveTime = 0.425f;
+    private float _moveTime = 0.395f;
     
     public override void Init()
     {
@@ -23,30 +21,4 @@ class MovementBehavior : SpellBehavior
         // _objectForSpell.GetComponent<ThirdPersonCharacter>().Move(_posDiff * _speed, false, false);
         _controller.Dash(_moveTime, _speed, _posDiff);
     }
-}
-
-class TimeSlowBehavior : SpellBehavior
-{
-    public float _duration;
-    public override void Init()
-    {
-        _speed = 0.2f;
-        _cooldown = 8f;
-
-        _duration = 1.5f;
-
-        _objectForSpell = null;
-    }
-    public override void SpellBehaviour(Spell spell)
-    {
-        var _durationScaled = _duration * _speed/0.2f;
-        Time.timeScale = _speed;
-        
-    }
-    
-    IEnumerator TimeSlow(float duaration, float speed)
-    {
-        yield return null;
-    }
-    
 }
