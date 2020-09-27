faddead6ff8a3ac82e409cd8059b9c17761ad185
Caleb <sonicheze8@gmail.com>
2020-09-27 21:14:59 +0800

    Add default spells in scriptable

---
+++ Assets/Scripts/UI/KeyCodeUI.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyCodeUI : MonoBehaviour
+{
+    public Sprite mouse1;
+    public Sprite mouse2;
+    public Sprite key;
+    public TextMeshProUGUI text;
+    private Image image;
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void SetKeyCode(KeyCode keyCode)
+    {
+        if (keyCode == KeyCode.Clear)
+        {
+            image.enabled = false;
+            text.text = "";
+            return;
+        }
+        image.enabled = true;
+
+        if (keyCode == KeyCode.Mouse0)
+        {
+            image.sprite = mouse1;
+            text.text = "";
+        }
+        else if (keyCode == KeyCode.Mouse1)
+        {
+            image.sprite = mouse2;
+            text.text = "";
+        }
+        else if (keyCode == KeyCode.Q)
+        {
+            image.sprite = key;
+            text.text = "Q";
+
+        }
+        else if (keyCode == KeyCode.E)
+        {
+            image.sprite = key;
+            text.text = "E";
+
+        }
+    }
+}
