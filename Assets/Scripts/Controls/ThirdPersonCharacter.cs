60494e50e00edfb1889abf28b5cddf6b1f2ce986
Caleb <sonicheze8@gmail.com>
2020-10-02 09:21:31 +0800

    Reset forward movement when UI is opened

--- Assets/Scripts/Controls/ThirdPersonCharacter.cs
+++ Assets/Scripts/Controls/ThirdPersonCharacter.cs
@@ -26,6 +26,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		float m_OrigGroundCheckDistance;
 		const float k_Half = 0.5f;
 		float m_TurnAmount;
+		[SerializeField]
 		float m_ForwardAmount;
 		Vector3 m_GroundNormal;
 		float m_CapsuleHeight;
@@ -42,6 +43,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		private static readonly int CastingShield = Animator.StringToHash("CastingShield");
 		private static readonly int CastingDash = Animator.StringToHash("CastingDash");
 		private static readonly int AnimationSpeed = Animator.StringToHash("AnimationSpeed");
+		private static readonly int Forward = Animator.StringToHash("Forward");
+		private static readonly int Turn = Animator.StringToHash("Turn");
+		private static readonly int OnGround = Animator.StringToHash("OnGround");
+		private static readonly int JumpLeg = Animator.StringToHash("JumpLeg");
 
 		public void Dash(float dashTime, float dashSpeed, Vector3 direction)
 		{
@@ -109,6 +114,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		public void Move(Vector3 move, bool crouch, bool jump, bool dashing =false)
 		{
+			
 			if (IsDisabled())
 			{
 				return;
@@ -135,8 +141,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				HandleAirborneMovement();
 			}
 
-			ScaleCapsuleForCrouching(crouch);
-			PreventStandingInLowHeadroom();
+			//ScaleCapsuleForCrouching(crouch);
+			//PreventStandingInLowHeadroom();
 
 			// send input and other state parameters to the animator
 			UpdateAnimator(move);
@@ -185,10 +191,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		void UpdateAnimator(Vector3 move)
 		{
 			// update the animator parameters
-			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.0f, Time.deltaTime);
-			m_Animator.SetFloat("Turn", m_TurnAmount, 0.0f, Time.deltaTime);
+			m_Animator.SetFloat(Forward, m_ForwardAmount, 0.0f, Time.deltaTime);
+			m_Animator.SetFloat(Turn, m_TurnAmount, 0.0f, Time.deltaTime);
 			m_Animator.SetBool("Crouch", m_Crouching);
-			m_Animator.SetBool("OnGround", m_IsGrounded);
+			m_Animator.SetBool(OnGround, m_IsGrounded);
 			if (!m_IsGrounded)
 			{
 				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
@@ -203,7 +209,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
 			if (m_IsGrounded)
 			{
-				m_Animator.SetFloat("JumpLeg", jumpLeg);
+				m_Animator.SetFloat(JumpLeg, jumpLeg);
 			}
 
 			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
@@ -241,6 +247,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				m_Animator.applyRootMotion = false;
 				m_GroundCheckDistance = 0.1f;
 			}
+			
 		}
 
 		void ApplyExtraTurnRotation()
@@ -255,7 +262,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		{
 			// we implement this function to override the default root motion.
 			// this allows us to modify the positional speed before it's applied.
-			if (m_IsGrounded && !m_Dashing &&Time.deltaTime > 0)
+			if (m_IsGrounded && !m_Dashing && Time.deltaTime > 0)
 			{
 				Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
 
@@ -263,6 +270,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				v.y = m_Rigidbody.velocity.y;
 				m_Rigidbody.velocity = v;
 			}
+			m_Animator.SetFloat(Forward, m_ForwardAmount, 0.0f, Time.deltaTime);
+			m_ForwardAmount = 0;
 		}
 
 
