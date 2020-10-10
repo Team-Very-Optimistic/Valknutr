532635ffec4f19c9a3fc100794543c989879275e
Caleb <sonicheze8@gmail.com>
2020-10-10 22:56:02 +0800

    Adaptive difficulty scaling system

--- Assets/Scripts/Scaling/DifficultyScalingSystem.cs
+++ Assets/Scripts/Scaling/DifficultyScalingSystem.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-namespace Scaling
+
+public class DifficultyScalingSystem : Singleton<DifficultyScalingSystem>
 {
-    public class DifficultyScalingSystem : MonoBehaviour
+    /// <summary>
+    /// Difficulty level 1 is easier, the higher it is the more difficult it gets
+    /// </summary>
+    public int difficultyLevel = 1;
+    public void Awake()
     {
-        
+        EnemyBehaviourBase.OnEnemyStart += ManageDifficulty;
+    }
+
+    private void ManageDifficulty(EnemyBehaviourBase enemyBehaviourBase)
+    {
+        IncreaseEnemyHealth(enemyBehaviourBase);
+    }
+
+    public void IncreaseEnemyHealth(EnemyBehaviourBase enemyBehaviourBase)
+    {
+        var hp = enemyBehaviourBase.GetComponent<HealthScript>();
+        hp.SetHealth(hp.maxHealth * difficultyLevel);
+    }
+
+    public void IncreaseDifficulty(int amount)
+    {
+        difficultyLevel += amount;
     }
 }
