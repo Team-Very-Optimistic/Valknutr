0c828733d5523f438bab7b6d522b049f93d6a4ec
Caleb <sonicheze8@gmail.com>
2020-10-11 11:46:36 +0800

    add prototype mods

---
+++ Assets/Scripts/Spell/Modifiers/RepeatSpellModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class RepeatSpellModifier : SpellModifier
+{
+    private int n = 2;
+    private SpellBase action;
+    public override SpellBase ModifyBehaviour(SpellBase action)
+    {
+        //important to make sure it doesnt cast a recursive method
+        Action oldBehavior = action.behaviour;
+        Action temp = () =>
+        {
+            oldBehavior.Invoke();
+            if (--n < 0) return;
+            var existingHandler = action._objectForSpell.GetComponent<TriggerEventHandler>();
+            if (existingHandler == null)
+                existingHandler = action._objectForSpell.AddComponent<TriggerEventHandler>();
+            existingHandler.AddEvent(Invoke);
+            this.action = action;
+        };
+
+        Action spell = temp;
+        action.behaviour = spell;
+        return action;
+    }
+
+    public void Invoke(Collider collider)
+    {
+        action._objectForSpell = collider.gameObject;
+        action.behaviour.Invoke();
+    }
+}
