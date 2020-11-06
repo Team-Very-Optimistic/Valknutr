ab60da4634743c47a341ed6d6086a5bb94d20f62
Caleb <sonicheze8@gmail.com>
2020-11-06 17:59:36 +0800

    Change how phasing lock works

---
+++ Assets/Scripts/Damage/PhasingDamageEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PhasingDamageEffect : DamageEffect
+{
+    private float timeLockDuration;
+
+    public PhasingDamageEffect SetDuration(float ls)
+    {
+        this.timeLockDuration = ls;
+        return this;
+    }
+
+    IEnumerator LockUnlock(NavMeshAgent agent)
+    {
+        // agent.enabled = false;
+        var speed = agent.speed;
+        agent.speed = 0;
+        yield return new WaitForSeconds(timeLockDuration);
+        if(agent)
+            agent.speed = speed;
+    }
+    public override void CastDamageEffect(Collider other, float damage)
+    {
+        var enemy = other.GetComponent<Enemy>();
+        if (!enemy) return;
+        
+        var ai = other.GetComponent<NavMeshAgent>();
+        EffectManager.PlayEffectAtPosition("RainbowEffect", other.transform.position);
+        AudioManager.PlaySoundAtPosition("lightBuff",  other.transform.position);
+        GameManager.Instance.StartCoroutine(LockUnlock(ai));
+
+    }
+    
+}
