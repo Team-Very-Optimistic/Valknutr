0a274ba732cb998116c3418ec95d14fa00b78e6c
Caleb <sonicheze8@gmail.com>
2020-11-08 10:17:50 +0800

    Add spider boss to optional level, add some spider webs in the level, clean level

---
+++ Assets/BossTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+ 
+///summary
+///summary
+public class BossTrigger : MonoBehaviour
+{
+ 
+    #region Public Fields
+
+    public GameObject boss;
+    #endregion
+ 
+    #region Unity Methods
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == GameManager.Instance._player)
+        {
+            StartCoroutine(SpawnBoss());
+        }
+    }
+
+    private IEnumerator SpawnBoss()
+    {
+        yield return new WaitForSeconds(0.3f);
+        boss.SetActive(true);
+    }
+
+    #endregion
+ 
+   
+}
