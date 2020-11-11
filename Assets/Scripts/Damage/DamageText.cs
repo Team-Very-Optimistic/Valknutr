3257ec002e2c111830a4fae8025d3201ba4c4b1f
Caleb <sonicheze8@gmail.com>
2020-11-11 16:18:17 +0800

    Reduce damage text size, make it not break

--- Assets/Scripts/Damage/DamageText.cs
+++ Assets/Scripts/Damage/DamageText.cs
@@ -20,7 +20,7 @@ public class DamageText : MonoBehaviour
     public float maxScale;
     public float horizontalScaleMultiplier;
     public float scaleReductionRate;
-    public const float BASE_DAMAGE = 3f;
+    public const float BASE_DAMAGE = 10f;
     
     void Start()
     {
@@ -74,7 +74,12 @@ public class DamageText : MonoBehaviour
     public void SetDamageTextProperties(float damage, Vector3 worldLocationStart, Color damageColor)
     {
         var scale = 1f;
-        if (damage < BASE_DAMAGE)
+        if (damage < BASE_DAMAGE/10)
+        {
+            // scale = Mathf.Pow(BASE_DAMAGE, damage - BASE_DAMAGE - 0.3f) + 0.3f;
+            scale = 0.5f;
+        }
+        else if (damage < BASE_DAMAGE)
         {
             // scale = Mathf.Pow(BASE_DAMAGE, damage - BASE_DAMAGE - 0.3f) + 0.3f;
             scale = 0.8f;
